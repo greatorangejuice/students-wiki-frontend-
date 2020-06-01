024cca78bca264b7fc6ff8b42a1f70ba6c69fdb4
Artem Korensky <a.korensky@brainq.com>
2020-06-01 20:34:42 +0300

    add @types/*

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import TestPage from "./routes/test-page";
+import TestComponent from "./components/test-component";
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <TestPage />
+        <TestComponent />
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
