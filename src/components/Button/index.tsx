0f04b5a17e01ae8d3c58bfcc6f939b8a5b81e9c9
Roman Samuilov <dronymys@mail.ru>
2020-06-05 21:05:19 +0300

    feat: add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import StyledButton from './styled';
+
+export interface Props {
+  className?: string;
+  children: string;
+  rest?: object;
+}
+
+const Button = ({ className, children, ...rest }: Props) => (
+  <StyledButton {...rest} className={className}>
+    {children}
+  </StyledButton>
+);
+
+export default Button;
